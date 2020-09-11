1aa73352da4d40ec0ff6006fec7d769c96c104f1
Jake Taylor <65513095+jakehtaylor@users.noreply.github.com>
2020-09-11 19:30:36 -0400

    date filter

--- static/js/UFO.js
+++ static/js/UFO.js
@@ -1,23 +1,48 @@
+
+var sightings = data;
+
 var tbody = d3.select("tbody");
 var head = d3.select("thead");
 
-var headers = Object.keys(data[0]);
+var headers = Object.keys(sightings[0]);
 var hrow = head.append("tr");
 
-headers.forEach(function(head) {
+headers.forEach(head => {
     var cell = hrow.append("th");
     cell.text(head);
 });
 
-data.forEach(function(sight) {
-    var row = tbody.append("tr");
-    Object.entries(sight).forEach(function([key, value]) {
-      var cell = row.append("td");
-      cell.text(value);
+function buildTable(data) {
+    data.forEach(sight => {
+        var row = tbody.append("tr");
+        Object.entries(sight).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+        });
     });
+};
+
+buildTable(sightings);
+
+var inputDate = d3.select("#date_in");
+var form = d3.select("#date_form");
+
+function filter_date() {
+    var date = d3.event.target.value
+    var filtered = sightings.filter(sight => sight.datetime === date);
+    console.log(filtered);
+    tbody.html("");
+    buildTable(filtered);
+};
+
+inputDate.on("change", filter_date);
+
+form.on("submit", function() {
+    d3.event.preventDefault();
 });
 
 
+  
 
 
 
