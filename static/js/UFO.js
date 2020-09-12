988e8c4f11cdd937dc2fd733cb3c5a3073e17b24
Jake Taylor <65513095+jakehtaylor@users.noreply.github.com>
2020-09-12 14:32:28 -0400

    stacked filters

--- static/js/UFO.js
+++ static/js/UFO.js
@@ -1,11 +1,13 @@
 
-var sightings = data;
+const sightings = data;
 
 var tbody = d3.select("tbody");
 var head = d3.select("thead");
 
 var headers = Object.keys(sightings[0]);
 var hrow = head.append("tr");
+
+filter_array = [];
 
 headers.forEach(head => {
     var cell = hrow.append("th");
@@ -31,18 +33,16 @@ var city = d3.select("#city");
 var shape = d3.select("#shape");
 var form = d3.select("#form");
 
-function filter_date() {
-    var date = d3.event.target.value
-    var filtered = sightings.filter(sight => sight.datetime === date);
-    tbody.html("");
-    buildTable(filtered);
+function filter_date(data) {
+    var date = d3.select("#date_in").property("value");
+    var filtered = data.filter(sight => sight.datetime === date);
+    return filtered;
 };
 
-function filter_country() {
-    var country = d3.event.target.value;
-    var filtered = sightings.filter(sight => sight.country === country);
-    tbody.html("");
-    buildTable(filtered);
+function filter_country(data) {
+    var country = d3.select("#country").property("value");
+    var filtered = data.filter(sight => sight.country === country);
+    return filtered;
 };
 
 function state_drop(){
@@ -59,11 +59,10 @@ function state_drop(){
 
 };
 
-function filter_state() {
-    var state = d3.event.target.value;
-    var filtered = sightings.filter(sight => sight.state === state);
-    tbody.html("");
-    buildTable(filtered);
+function filter_state(data) {
+    var state = d3.select("#state").property("value");
+    var filtered = data.filter(sight => sight.state === state);
+    return filtered;
 };
 
 function city_drop(){
@@ -80,49 +79,72 @@ function city_drop(){
 
 };
 
-function filter_city() {
-    var city = d3.event.target.value;
-    var filtered = sightings.filter(sight => sight.city === city);
-    tbody.html("");
-    buildTable(filtered);
+function filter_city(data) {
+    var city = d3.select("#city").property("value");
+    var filtered = data.filter(sight => sight.city === city);
+    return filtered;
 };
 
 var shapes = sightings.map(s => s.shape);
 var u_shapes = [... new Set(shapes)];
 u_shapes.forEach(sh => {
-    console.log(sh);
     var o = shape.append("option");
         o.text(sh);
 });
 
-function filter_shape() {
-    var shape = d3.event.target.value;
-    var filtered = sightings.filter(sight => sight.shape === shape);
-    tbody.html("");
-    buildTable(filtered);
+function filter_shape(data) {
+    var shape = d3.select("#shape").property("value");
+    var filtered = data.filter(sight => sight.shape === shape);
+    return filtered;
 };
 
-inputDate.on("change", filter_date);
-shape.on("change", filter_shape);
+inputDate.on("change", function() {
+    filter_array.push(filter_date);
+    filter();
+    
+});
+shape.on("change", function() {
+    filter_array.push(filter_shape);
+    filter();
+    
+});
 
 country.on("change", function () {
-    filter_country();
+    filter_array.push(filter_country);
+    filter();
     state_drop();
 });
 
 state.on("change", function () {
-    filter_state();
+    filter_array.push(filter_state);
+    filter();
     city_drop();
 });
 
-city.on("change", filter_city)
+city.on("change", function() {
+    filter_array.push(filter_city);
+    
+    filter();
+});
 
 form.on("submit", function() {
     d3.event.preventDefault();
 });
 
+function filter() {
+    var table_data = sightings;
 
-  
+    filter_array.forEach(fn => {
+        table_data = fn(table_data);
+    });
+
+    tbody.html("");
+    buildTable(table_data);
+};
+
+
+
+
 
 
 
