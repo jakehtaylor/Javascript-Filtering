934b2632a87a7d648f6a581cbe9a88cd283f4983
Jake Taylor <jakeholdentaylor@gmail.com>
2020-11-28 18:12:02 -0500

    commented code

--- static/js/UFO.js
+++ static/js/UFO.js
@@ -1,18 +1,24 @@
 
 const sightings = data;
 
+// data provided in the data.js file
+
 var tbody = d3.select("tbody");
-var head = d3.select("thead");
+var head = d3.select("thead"); 
+
+// selecting table elements with d3
 
 var headers = Object.keys(sightings[0]);
 var hrow = head.append("tr");
 
-filter_array = [];
+// loop to add each column header
 
 headers.forEach(head => {
     var cell = hrow.append("th");
     cell.text(head);
 });
+
+// function that builds out a table from given data
 
 function buildTable(data) {
     data.forEach(sight => {
@@ -24,7 +30,11 @@ function buildTable(data) {
     });
 };
 
+// creates unfiltered table when the page is loaded.
+
 buildTable(data);
+
+// retrieving each of the filter items by id
 
 var inputDate = d3.select("#date_in");
 var country = d3.select("#country");
@@ -33,12 +43,18 @@ var city = d3.select("#city");
 var shape = d3.select("#shape");
 var form = d3.select("#form");
 
+// populating the shape drop down with all of the unique shape values
+
 var shapes = sightings.map(s => s.shape);
 var u_shapes = [... new Set(shapes)];
 u_shapes.forEach(sh => {
     var o = shape.append("option");
         o.text(sh);
 });
+
+// following are the basic filter functions for each parameter, returning an unchanged copy of the data if
+// the value of the filter is the default/'all' value, and returning a filtered version of the data if an option
+// other than the default has been selected, containing matches with the selected option.
 
 function filter_shape(data) {
     var shape = d3.select("#shape").property("value");
@@ -90,6 +106,11 @@ function filter_city(data) {
 };
 
 
+// below are the two functions used to create the progressive specificity location filters. The two functions
+// follow all the same steps: find the current value of the preceding layer (country for state, state for city),
+// filter the data on that preceding layer, find the unique values of the current layer (state or city), and 
+// populate the drop down associated with the current layer with those unique values.
+
 function state_drop(){
     state.html("<option selected>all</option>");
     var country = d3.event.target.value;
@@ -118,7 +139,11 @@ function city_drop(){
 
 };
 
+// array of filtering functions 
 
+filter_array = [];
+
+// on any change of the filters, the associated filter function gets pushed to the filter array
 
 inputDate.on("change", function() {
     filter_array.push(filter_date);
@@ -148,9 +173,14 @@ city.on("change", function() {
     filter();
 });
 
+// prevents the page from fully reloading on form submission
+
 form.on("submit", function() {
     d3.event.preventDefault();
 });
+
+// this final function applies each filter in the filter array to the original data in succession, 
+// stacking the filters on top of each other.
 
 function filter() {
     var table_data = sightings;
